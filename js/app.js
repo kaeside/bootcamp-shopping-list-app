654e5f7ada864a0b9b53bb4a7c2cafc2db3a01a9
Kaeside Iwuagwu <kaeside.iwuagwu@gmail.com>
2016-04-12 11:55:53 -0400

    Add listener that returns an alert for text entry to shopping list input field to app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-
-})
-
-
+// Button-click functionality
+$("#add-item").click(function(){
+	var shoppingItem = document.getElementByID('#user-input');
+	alert(shoppingItem);
+});
 
 // Put text input into a variable
-
-
 
 // Append text input variable into list
 
 // Prepend li from list
+});
+
+
+
