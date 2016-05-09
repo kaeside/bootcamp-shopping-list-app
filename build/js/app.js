f82e6c5c38439d13972957bee8b63f2bf5a9afe0
Elias Mason <eliasjmason@gmail.com>
2016-05-09 11:50:47 -0700

    Fixes appemd error caused by name change edits.

--- build/js/app.js
+++ build/js/app.js
@@ -1,41 +1,20 @@
 $(document).ready(function () {
-
-// METHOD KYLE AND KAESIDE USED
-// use enter to submit
-    // $('#user-input').keydown(function (e) {
-    // 	console.log("hello")
-    //     if (e.keyCode == 13) {
-    //         $("#add-item").submit();
-    //     }
-    // });
-
-//SIMPLER "SUBMIT" METHOD
-// Button-click functionality
-    $("#add-item").submit(function (event) {
+    $(".add-item").submit(function (event) {
     	event.preventDefault();
-        // Put text input into a variable
-        var shoppingItem = $('#user-input').val();
-        // var userInput = document.getElementById('user-input');
-        // var shoppingItem = userInput.value;
+        var shoppingItem = $('.user-input').val();
 
         if (shoppingItem === '') {
             alert("Please enter an item");
         }
         else {
-            // Append text input variable into list
-            $('#shoppingList').append('<li>' + shoppingItem + ' ' + '<button class ="btn btn-danger delete">Delete Item</button></li>');
-			$('#user-input').val("");
-            // userInput.value = '';
+            $('.shopping-list').append('<li>' + shoppingItem + ' ' + '<button class ="btn btn-danger delete">Delete Item</button></li>');
+			$('.user-input').val("");
 
 
         }
     });
 
-// Delete list item
-    $('#shoppingList').on("click", ".delete", function () {
+    $('.shopping-list').on("click", ".delete", function () {
         $(this).parent().remove();
     });
 });
-
-
-// Prepend li from list
