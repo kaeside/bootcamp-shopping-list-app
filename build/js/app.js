c41e25d7e5fe42a3835334da2ab6f0c93f3f2dc7
Kaeside Iwuagwu <kaeside.iwuagwu@gmail.com>
2016-05-09 13:19:00 -0400

    add js and images directories to build process

---
+++ build/js/app.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+// METHOD KYLE AND KAESIDE USED
+// use enter to submit
+    // $('#user-input').keydown(function (e) {
+    // 	console.log("hello")
+    //     if (e.keyCode == 13) {
+    //         $("#add-item").submit();
+    //     }
+    // });
+
+//SIMPLER "SUBMIT" METHOD
+// Button-click functionality
+    $("#add-item").submit(function (event) {
+    	event.preventDefault();
+        // Put text input into a variable
+        var shoppingItem = $('#user-input').val();
+        // var userInput = document.getElementById('user-input');
+        // var shoppingItem = userInput.value;
+
+        if (shoppingItem === '') {
+            alert("Please enter an item");
+        }
+        else {
+            // Append text input variable into list
+            $('#shoppingList').append('<li>' + shoppingItem + ' ' + '<button class ="btn btn-danger delete">Delete Item</button></li>');
+			$('#user-input').val("");
+            // userInput.value = '';
+
+
+        }
+    });
+
+// Delete list item
+    $('#shoppingList').on("click", ".delete", function () {
+        $(this).parent().remove();
+    });
+});
+
+
+// Prepend li from list
